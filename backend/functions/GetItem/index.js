166ab8be1b8fd85c09758866bfeaaa776eb5fabd
NajibMuhammadi <najibmuhammadi003@gmail.com>
2024-11-11 14:38:21 +0100

    DeleteITem och GetItem är good to go

--- backend/functions/GetItem/index.js
+++ backend/functions/GetItem/index.js
@@ -1,7 +1,18 @@
+const {db} = require('../../services/index.js')
+const {sendResponse, sendError } = require('../../responses/index')
+
 exports.handler = async (event) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+   try{
+    const data = await db.scan({
+        TableName: 'conezonemenu-db',
+    })
+    return sendResponse(200, data.Items);
+   }catch(err){
+        return sendError(404, {message : err.message});
+   }
 }
 
+/* 
+    * Författare: Najib
+    * Funktion som hämtar alla items från vår meny databas
+ */
