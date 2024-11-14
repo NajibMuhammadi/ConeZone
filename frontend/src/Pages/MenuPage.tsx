c89d157f388548566ec1fe6b7e8863d767f422c9
NajibMuhammadi <najibmuhammadi003@gmail.com>
2024-11-14 09:36:12 +0100

    klar med cartPage förutom resposive

--- frontend/src/Pages/MenuPage.tsx
+++ frontend/src/Pages/MenuPage.tsx
@@ -1,5 +1,5 @@
-import Footer from "../Components/Footer"
-import Header from "../Components/Header"
+import Footer from "../components/Footer"
+import Header from "../components/Header"
 import './styles/menuPage.css';
 
 import { useEffect, useState } from "react";
