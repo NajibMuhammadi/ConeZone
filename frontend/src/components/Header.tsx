126eb926139d92b40da9146ecbd9163d84448351
Ida Walther <idahwalther@icloud.com>
2024-11-14 11:48:19 +0100

    Fixed the mobile view of the header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,34 @@
 import { NavLink, NavLinkRenderProps } from 'react-router-dom';
+import { useState } from 'react';
 import './styles/header.css'
 
 function Header() {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+
+    const toggleNav = () : void => {
+        setIsOpen(!isOpen)
+    }
+
     return (
         <header className="header">
+            <button className="header__button" onClick={toggleNav}><img className="button__hamburger" src="../../src/assets/hamburger.svg"/></button>
             <nav className="header__nav">
                 <NavLink to="/" className={({ isActive } : NavLinkRenderProps) => isActive ? 'nav__link nav__link--active' : 'nav__link'}><img className="header__logo" src="../../../src/assets/logo.svg"></img></NavLink>
-                    <NavLink to="/menu" className={({ isActive } : NavLinkRenderProps) => isActive ? 'nav__link nav__link--active' : 'nav__link'}>Menu</NavLink>
-                    <NavLink to="/about" className={({ isActive } : NavLinkRenderProps) => isActive ? 'nav__link nav__link--active' : 'nav__link'}>About Us</NavLink>
-                    <NavLink to="/login" className={({ isActive } : NavLinkRenderProps) => isActive ? 'nav__link nav__link--active' : 'nav__link'}>Login</NavLink>
+                <NavLink to="/menu" className={({ isActive } : NavLinkRenderProps) => isActive ? 'nav__link nav__link--active' : 'nav__link'}>Menu</NavLink>
+                <NavLink to="/about" className={({ isActive } : NavLinkRenderProps) => isActive ? 'nav__link nav__link--active' : 'nav__link'}>About Us</NavLink>
+                <NavLink to="/login" className={({ isActive } : NavLinkRenderProps) => isActive ? 'nav__link nav__link--active' : 'nav__link'}>Login</NavLink>
                 <NavLink to="/cart" className={({ isActive } : NavLinkRenderProps) => isActive ? 'nav__link nav__link--active' : 'nav__link'}><img className="header__basket" src="../../src/assets/basket.svg"></img></NavLink>
+            </nav>
+            <nav className="header__nav header__nav--mobile">
+                <NavLink to="/" className={({ isActive } : NavLinkRenderProps) => isActive ? 'nav__link nav__link--active' : 'nav__link'}><img className="header__logo" src="../../../src/assets/logo.svg"></img></NavLink>
+                    <section className={`header__section ${isOpen ? 'open' : ''}`}>
+                        <NavLink to="/menu" className={({ isActive } : NavLinkRenderProps) => isActive ? 'nav__link nav__link--active' : 'nav__link'}>Menu</NavLink>
+                        <NavLink to="/about" className={({ isActive } : NavLinkRenderProps) => isActive ? 'nav__link nav__link--active' : 'nav__link'}>About Us</NavLink>
+                        <NavLink to="/login" className={({ isActive } : NavLinkRenderProps) => isActive ? 'nav__link nav__link--active' : 'nav__link'}>Login</NavLink>
+                        <NavLink to="/cart" className={({ isActive } : NavLinkRenderProps) => isActive ? 'nav__link nav__link--active' : 'nav__link'}><img className="header__basket" src="../../src/assets/basket.svg"></img></NavLink>
+                    </section>
             </nav>
         </header>
     )
@@ -22,4 +41,5 @@ export default Header;
  * En funktion som sköter navigeringen på sidan. 
  * Har använt NavLink för att kunna hålla reda på vilken sida som är aktiv och inte.
  * Om sidan är aktiv får den klassen nav__link OCH nav__link--active annars bara nav__link
+ * Skapade även en annan Nav för mobil view
  */
