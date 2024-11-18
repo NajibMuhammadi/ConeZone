8a9fdbc424d204d598b8a984e575371834453df8
Ida Walther <idahwalther@icloud.com>
2024-11-18 16:11:46 +0100

    Added the newOrder function together with a validateNewOrder and a orderSchema

---
+++ backend/functions/newOrder/index.js
@@ -0,0 +1,39 @@
+const {db} = require('../../services/index.js')
+const {sendResponse, sendError } = require('../../responses/index')
+const {v4 : uuid} = require('uuid')
+const middy = require('@middy/core');
+const {errorHandler} = require('../../middlewares/errorHandler.js')
+const {validateNewOrder} = require('../../middlewares/validateNewOrder.js')
+
+const handler = async (event) => {
+    const {username, name, mail, number, orderDetails, totalPrice} = JSON.parse(event.body)
+    const id = uuid();
+
+    try {
+        await db.put({
+            TableName: 'conezoneorder-db',
+            Item: {
+                pk: username || 'guest',
+                sk: id,
+                name: name,
+                mail: mail,
+                number: number,
+                order: orderDetails,
+                totalPrice: totalPrice 
+        }
+    })
+        return sendResponse(200, {success: true, message: 'New order added'})
+    } catch (error) {
+        return sendError (500, {success: false, message: error.message})
+    }
+}
+
+const middyHandler = middy(handler);
+exports.handler = middyHandler.use(validateNewOrder()).use(errorHandler());
+
+/**
+* Författare: Ida
+* Funktion som lägger till ordrar till vår order databas.
+* Om användaren är inloggad lagrats det under användarnamnet, annars under guest. 
+* Funtionen använder sig av middy för att dels validera den inkommande datan mot en valideringsmiddleware och eventuella fel hanteras av errorHandlern
+*/
---
+++ backend/models/orderSchema.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+
+const orderSchema = Joi.object({
+    pk : Joi.string().required(),
+    name : Joi.string().required(),
+    mail : Joi.string().email().required(),
+    number: Joi.string().pattern(/^[0-9]+$/).required(),
+    order: Joi.array().items(
+        Joi.object({
+            name: Joi.string().required(),
+            itemPrice: Joi.number().strict().required(),
+            quantity: Joi.number().strict().required(),
+            itemTotal: Joi.number().strict().required()
+        })).required(),
+    totalPrice : Joi.number().strict().required()
+}); 
+
+module.exports = { orderSchema };
+
+/**
+ * Författare Ida
+ * Skapar en JoiSchema där jag säkerhetsställer att nya order är av rätt typ och är required.
+ */
